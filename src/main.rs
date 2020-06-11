10d382a84cd7942aeee7724c11f4216147c843df
Blake Wyatt <nabakin@protonmail.ch>
2020-06-10 21:48:47 -0400

    Add boilerplate

--- src/main.rs
+++ src/main.rs
@@ -9,7 +9,7 @@ fn main() {
     for stream in listener.incoming() {
         let stream = stream.unwrap();
 
-        println!("Connection established!");
+        println!("\nConnection established!\n");
         handle_connection(stream);
     }
 }
@@ -18,10 +18,47 @@ fn handle_connection(mut stream: TcpStream) {
     let mut buffer = [0; 512];
     stream.read(&mut buffer).unwrap();
 
-    let contents = fs::read_to_string("static/index.html").unwrap();
+    let buffer_str = String::from_utf8_lossy(&buffer[..]);
+    println!("Request: {}", buffer_str);
 
-    let response = format!("HTTP/1.1 200 OK\r\n\r\n{}", contents);
+    let mut buffer_str_iter = buffer_str.split(" ");
+    let method = buffer_str_iter.next().unwrap();
+    let location = buffer_str_iter.next().unwrap();
+
+    let response = match method {
+        "GET" => get_request(location),
+        _ => respond_400()
+    };
+    println!("Response: {}", response);
 
     stream.write(response.as_bytes()).unwrap();
     stream.flush().unwrap();
+}
+
+fn get_request(location: &str) -> String {
+    match location {
+        "/" => respond_static("index.html"),
+        _ => respond_404()
+    }
+}
+
+fn respond_static(filepath: &str) -> String {
+    let status_line = "HTTP/1.1 200 OK\r\n\r\n";
+    let payload = fs::read_to_string("static/".to_string() + filepath).unwrap();
+
+    status_line.to_string() + &payload
+}
+
+fn respond_400() -> String {
+    let status_line = "HTTP/1.1 400 BAD REQUEST\r\n\r\n";
+    let payload = fs::read_to_string("static/responses/400.html").unwrap();
+
+    status_line.to_string() + &payload
+}
+
+fn respond_404() -> String {
+    let status_line = "HTTP/1.1 404 NOT FOUND\r\n\r\n";
+    let payload = fs::read_to_string("static/responses/404.html").unwrap();
+
+    status_line.to_string() + &payload
 }
